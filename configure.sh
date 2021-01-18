15dd5dfeeb552124b7bfd6774935d7d1c6cabf83
Tyler Chen <tylerc6@illinois.edu>
2021-01-17 18:19:18 -0600

    add more resource tests

--- configure.sh
+++ configure.sh
@@ -2,6 +2,6 @@
 echo "Creating cmake build files:"
 cmake -S . -B out -DCMAKE_BUILD_TYPE=Debug
 echo "Copying resource directories:"
-cp -r demo/res out/demo/res
-cp -r test/shaders out/test/
+cp -r demo/res out/demo/
+cp -r test/res out/test/
 echo "Done!"
--- test/resourcetest.cpp
+++ test/resourcetest.cpp
@@ -33,10 +33,36 @@ TEST_CASE("Does not duplicate the same resources", "[resource]") {
   REQUIRE(foo1->use_count() == 3); // two for these two references, one for the manager itself
 }
 
+TEST_CASE("Cleans up after unused resources", "[resource]") {
+  using namespace laika3d;
+  ResourceManager rm;
+  {
+    auto foo = rm.load<MockResource>("foo");
+  }
+  auto bar = rm.load<MockResource>("bar");
+  REQUIRE(rm.get_resource_count() == 1);
+}
+
 TEST_CASE("Can load actual shaders", "[resource]") {
   using namespace laika3d;
   Renderer ren(100, 100);
   ResourceManager rm;
-  auto shader = rm.load<Shader>("shaders/simple.glsl");
+  auto shader = rm.load<Shader>("res/simple.glsl");
   REQUIRE(shader.has_value());
+}
+
+TEST_CASE("Can load actual textures", "[resource]") {
+  using namespace laika3d;
+  Renderer ren(100, 100);
+  ResourceManager rm;
+  auto texture = rm.load<Texture>("res/test.png");
+  REQUIRE(texture.has_value());
+}
+
+TEST_CASE("Can load actual meshes", "[resource]") {
+  using namespace laika3d;
+  Renderer ren(100, 100);
+  ResourceManager rm;
+  auto mesh = rm.load<Mesh>("res/test.obj");
+  REQUIRE(mesh.has_value());
 }
