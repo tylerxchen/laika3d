bd02b94f464172dd9ed92d91f95075a417a6e637
Tyler Chen <tylerc6@illinois.edu>
2021-03-06 21:37:51 -0500

    add specular lighting

--- demo/res/shaders/shader.glsl
+++ demo/res/shaders/shader.glsl
@@ -8,6 +8,7 @@ layout(location = 2) in vec2 texture_coordinate;
 out vec2 uv;
 out vec3 frag_normal;
 out vec3 frag_position;
+out vec3 light_position;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -17,8 +18,10 @@ void main() {
   gl_Position = proj * view * model * vec4(vertex_position, 1.0);
 
   uv = texture_coordinate;
-  frag_normal = vertex_normal;
-  frag_position = vec3(model * vec4(vertex_position, 1.0));
+  frag_normal = mat3(transpose(inverse(view * model))) * vertex_normal;
+  frag_position = vec3(view * model * vec4(vertex_position, 1.0));
+  // there's a hard coded light at (0.0, 0.0, 0.0) for the time being
+  light_position = vec3(view * vec4(0.0, 0.0, 0.0, 1.0));
 }
 
 #FRAGMENT
@@ -27,6 +30,7 @@ void main() {
 in vec2 uv;
 in vec3 frag_normal;
 in vec3 frag_position;
+in vec3 light_position;
 
 out vec4 color;
 
@@ -34,17 +38,26 @@ uniform sampler2D texture_sampler;
 
 void main() {
   vec4 tex_color = texture(texture_sampler, uv);
-  vec3 norm = normalize(frag_normal);
 
-  // there's a hard coded light at (0.0, 0.0, 0.0) for the time being
+  // ambient
   float ambient_str = 0.3;
   vec3 light_color = vec3(1.0, 1.0, 1.0);
-  vec3 light_pos = vec3(0.0, 0.0, 0.0);
-  vec3 light_dir = normalize(light_pos - frag_position);
-  float diff = max(dot(norm, light_dir), 0.0);
-
-  vec3 diffuse = diff * light_color;
   vec3 ambient = ambient_str * light_color;
 
-  color = (vec4(ambient, 1.0) + vec4(diffuse, 1.0)) * tex_color;
+  vec3 light_dir = normalize(light_position - frag_position);
+
+  // diffuse
+  vec3 norm = normalize(frag_normal);
+  float diff = max(dot(norm, light_dir), 0.0);
+  vec3 diffuse = diff * light_color;
+
+  // specular
+  float specular_str = 0.5;
+  int shininess = 32;
+  vec3 view_dir = normalize(-frag_position);
+  vec3 halfway_dir = normalize(light_dir + view_dir);
+  float spec = pow(max(dot(frag_normal, halfway_dir), 0.0), shininess);
+  vec3 specular = light_color * spec;
+
+  color = (vec4(ambient, 1.0) + vec4(diffuse, 1.0) + vec4(specular, 1.0)) * tex_color;
 }
--- src/scene/geometrynode.hpp
+++ src/scene/geometrynode.hpp
@@ -16,6 +16,7 @@ namespace laika3d {
       void unbind() const;
 
       void set_mvp(const glm::mat4& model, const glm::mat4& view, const glm::mat4& proj);
+
       std::shared_ptr<Mesh> mesh;    
       std::shared_ptr<Texture> texture;
       std::shared_ptr<Shader> shader;
